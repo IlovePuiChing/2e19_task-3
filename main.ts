55127986a08e5d91b6e0b3b3fdf7666c121afc22
IlovePuiChing <115064795+IlovePuiChing@users.noreply.github.com>
2022-10-05 14:46:28 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(1)
+    basic.showNumber(2)
+    basic.showNumber(3)
+    basic.showNumber(4)
+    basic.showNumber(5)
+    basic.showLeds(`
+        . . # # .
+        . # . . .
+        . # # # #
+        . # . . #
+        . . # # .
+        `)
 })
